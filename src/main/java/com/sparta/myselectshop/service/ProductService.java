32e26e14a4a0437ffbc14eba0d3744597ef5962b
phdljr <phdljr@naver.com>
2023-11-13 16:12:53 +0900

    feat: add search favorite products

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -5,6 +5,8 @@ import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,5 +40,16 @@ public class ProductService {
         product.update(requestDto);
 
         return new ProductResponseDto(product);
+    }
+
+    public List<ProductResponseDto> getProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductResponseDto> result = new ArrayList<>();
+
+        for (Product product : products) {
+            result.add(new ProductResponseDto(product));
+        }
+
+        return result;
     }
 }
