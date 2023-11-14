9fa251304721dc94bce9aebe4881618c9e141730
phdljr <phdljr@naver.com>
2023-11-14 10:57:33 +0900

    feat: add search products in folder

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -11,5 +11,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByUser(User user, Pageable pageable);
 
+    /*
+        select p.id, pf.folder_id
+            from product p
+                left join product_folder pf
+                    on p.id = pf.product_id
+            where p.user_id = 1
+                and
+                pf.folder_id = 3
+            order by p.id desc
+                limit 10, 10; => 페이지네이션
+     */
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
