d6dcee50010db9c84a9bb5b61c844da3410b08f0
phdljr <phdljr@naver.com>
2023-11-30 14:13:04 +0900

    Feat: ControllerAdvice 적용

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -82,15 +82,4 @@ public class ProductController {
             userDetails.getUser()
         );
     }
-
-    @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
-        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(
-            // HTTP body
-            restApiException,
-            // HTTP status code
-            HttpStatus.BAD_REQUEST
-        );
-    }
 }
