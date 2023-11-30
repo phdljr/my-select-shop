beaa68c1e4198da5b1575e07912ec4256c5b40a1
phdljr <phdljr@naver.com>
2023-11-30 19:49:07 +0900

    Feat: Error 메세지 관리

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -8,12 +8,15 @@ import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.ProductFolder;
 import com.sparta.myselectshop.entity.User;
 import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.exception.ProductNotFoundException;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.FolderRepository;
 import com.sparta.myselectshop.repository.ProductFolderRepository;
 import com.sparta.myselectshop.repository.ProductRepository;
+import java.util.Locale;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -29,6 +32,7 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ProductFolderRepository productFolderRepository;
     private final FolderRepository folderRepository;
+    private final MessageSource messageSource;
 
     public static final int MIN_MY_PRICE = 100;
 
@@ -43,11 +47,23 @@ public class ProductService {
         int myprice = requestDto.getMyprice();
         if (myprice < MIN_MY_PRICE) {
             throw new IllegalArgumentException(
-                "유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+                messageSource.getMessage(
+                    "below.min.my.price",
+                    new Integer[]{MIN_MY_PRICE},
+                    "Wrong Price",
+                    Locale.getDefault())
+            );
         }
 
         Product product = productRepository.findById(id).orElseThrow(() ->
-            new NullPointerException("해당 상품을 찾을 수 없습니다.")
+            new ProductNotFoundException(
+                messageSource.getMessage(
+                    "not.found.product",
+                    null,
+                    "Not Found Product",
+                    Locale.getDefault()
+                )
+            )
         );
 
         product.update(requestDto);
